7b30bddb613e4d819f1aa6a4fea26acd31983b4b
UnknownXx010 <athag90@gmail.com>
2026-02-27 09:13:27 +0700

    update

---
+++ buat_user.php
@@ -0,0 +1,9 @@
+<?php
+include "koneksi.php";
+
+$username = "admin";
+$password = password_hash("123456", PASSWORD_DEFAULT);
+
+mysqli_query($conn, "INSERT INTO users (username,password) VALUES ('$username','$password')");
+echo "User berhasil dibuat";
+?>
